4c9f0160170b9b53abba0860aa7f50c469801e08
Faizanriaz44553 <sylanipractice@gmail.com>
2026-07-29 15:17:47 -0700

    project error resoleved

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import {BrowserRouter} from 'react-router-dom';
-import { AuthProvider } from './context/authContext.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
