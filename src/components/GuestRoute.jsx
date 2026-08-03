f2edd5bec8a38c6ab62422bb92531f9ab9fba64f
Faizanriaz44553 <sylanipractice@gmail.com>
2026-08-03 15:23:01 -0700

    updated

--- src/components/GuestRoute.jsx
+++ src/components/GuestRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 import Customloader from "./UI/Customloader";
+import { useAuth } from "../context/AuthContext";
 
 const GuestRoute = ({ children }) => {
   const { user, loading } = useAuth();
